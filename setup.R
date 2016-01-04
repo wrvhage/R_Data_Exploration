c3c26441fedd8ce142327c11f630d252b050348f
Willem van Hage <w.vanhage@esciencecenter.nl>
2016-01-04 13:45:15 +0100

    Added packrat packages for 'dplyr','tidyr','stringr','zoo','xts','lubridate','ggplot2','ggmap','textcat','data.table','jsonlite','XML','foreign','XLConnect','RODBC','RPostgreSQL','RSQLite'.

---
+++ setup.R
@@ -0,0 +1,8 @@
+packages <- c('dplyr','tidyr','stringr',
+              'zoo','xts','lubridate',
+              'ggplot2','ggmap',
+              'textcat',
+              'data.table',
+              'jsonlite','XML','foreign',
+              'XLConnect','RODBC','RPostgreSQL','RSQLite')
+install.packages(packages,dependencies=TRUE)
